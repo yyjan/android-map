a7f817e4a3ca5f0951cdf614f251e7fd15f4bf92
yun <yyjan219@gmail.com>
2020-05-07 18:51:43 +0900

    feat: delete naver maps sdk

--- app/src/main/java/com/yun/maskdetector/MainActivity.kt
+++ app/src/main/java/com/yun/maskdetector/MainActivity.kt
@@ -1,53 +1,22 @@
 package com.yun.maskdetector
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.naver.maps.map.LocationTrackingMode
-import com.naver.maps.map.MapFragment
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
-import com.naver.maps.map.util.FusedLocationSource
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModel()
-
-    private var locationSource: FusedLocationSource? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as MapFragment
-        mapFragment.getMapAsync(this)
-
-        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (locationSource?.onRequestPermissionsResult(requestCode, permissions, grantResults) == true) {
-            return
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //viewModel.fetchStores(location.latitude.toFloat(), location.longitude.toFloat(), 5000)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        locationSource = null
-    }
-
-    override fun onMapReady(naverMap: NaverMap) {
-        naverMap.locationSource = locationSource
-        naverMap.locationTrackingMode = LocationTrackingMode.Follow
-
-        naverMap.addOnLocationChangeListener { location ->
-            val locationString = location.latitude.toString() + "," + location.longitude.toString()
-            Toast.makeText(this, locationString, Toast.LENGTH_SHORT).show()
-
-            viewModel.fetchStores(location.latitude.toFloat(), location.longitude.toFloat(), 5000)
-        }
     }
 
     companion object {
