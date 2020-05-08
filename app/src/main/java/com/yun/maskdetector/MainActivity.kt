feff5f17573f8a5fce34a5e7ec4d30208944eeb8
yun <yyjan219@gmail.com>
2020-05-08 18:45:05 +0900

    feat: setup google maps sdk

--- app/src/main/java/com/yun/maskdetector/MainActivity.kt
+++ app/src/main/java/com/yun/maskdetector/MainActivity.kt
@@ -2,24 +2,41 @@ package com.yun.maskdetector
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.coroutineScope
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.ktx.MapsExperimentalFeature
+import com.google.maps.android.ktx.awaitMap
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: MainViewModel by viewModel()
 
+    private lateinit var map: GoogleMap
+
+    @MapsExperimentalFeature
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //viewModel.fetchStores(location.latitude.toFloat(), location.longitude.toFloat(), 5000)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        lifecycle.coroutineScope.launchWhenCreated {
+            check(mapFragment != null)
+            
+            map = mapFragment.awaitMap()
+            with(map) {
+                moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, ZOOM_LEVEL))
+                addMarker(MarkerOptions().position(SEOUL))
+            }
+        }
     }
 
     companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+        private val SEOUL = LatLng(37.566682, 126.978596)
+        private const val ZOOM_LEVEL = 17f
     }
 }
