7e13ad4af3968a5367027b11d80c11f275f2e366
yun <yyjan219@gmail.com>
2020-05-19 18:33:33 +0900

    feat: get current location using FusedLocationProviderClient

--- app/src/main/java/com/yun/maskdetector/MainActivity.kt
+++ app/src/main/java/com/yun/maskdetector/MainActivity.kt
@@ -3,19 +3,22 @@ package com.yun.maskdetector
 import android.Manifest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.maps.android.ktx.MapsExperimentalFeature
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import pub.devrel.easypermissions.EasyPermissions
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback{
 
     private val viewModel: MainViewModel by viewModel()
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private lateinit var map: GoogleMap
 
@@ -24,7 +27,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
     }
 
@@ -40,16 +46,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
-        updateLocationUI()
-    }
-
-    private fun hasLocationPermission(): Boolean {
-        return EasyPermissions.hasPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        getLastLocation()
     }
 
     private fun enableLocation() {
         if (hasLocationPermission()) {
-            updateLocationUI()
+            getLastLocation()
         } else {
             EasyPermissions.requestPermissions(
                 this, getString(R.string.location),
@@ -58,12 +60,27 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun updateLocationUI() {
+    private fun hasLocationPermission(): Boolean {
+        return EasyPermissions.hasPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+
+    private fun getLastLocation() {
+        fusedLocationClient.lastLocation
+            .addOnCompleteListener { taskLocation ->
+                if (taskLocation.isSuccessful && taskLocation.result != null) {
+                    taskLocation.result?.run {
+                        val latLng = LatLng(latitude, longitude)
+                        updateLocationUI(latLng)
+                    }
+                }
+            }
+    }
+
+    private fun updateLocationUI(latLng: LatLng) {
         map.run {
             isMyLocationEnabled = true
             uiSettings.isMyLocationButtonEnabled = true
-            moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, ZOOM_LEVEL))
-            addMarker(MarkerOptions().position(SEOUL))
+            moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, ZOOM_LEVEL))
         }
     }
 
